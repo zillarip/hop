14a356e00aa6a9c3e25b5c831d63bb9176faaed2
Hydrogen <34059898+thehydrogen@users.noreply.github.com>
2022-10-24 22:07:52 +0200

    git did a goof

--- Bukkit/src/main/java/me/hydro/queue/listeners/MessageListener.java
+++ Bukkit/src/main/java/me/hydro/queue/listeners/MessageListener.java
@@ -13,21 +13,24 @@ import org.bukkit.event.Listener;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class MessageListener implements Listener {
 
     @EventHandler
     public void onRedisMessage(RedisMessageEvent event) {
-        String channel = event.getChannel();
-        String message = event.getMessage();
+        final String channel = event.getChannel();
+        final String message = event.getMessage();
 
         switch (channel) {
             case "ALLOW": {
-                String server = message;
-                Queue queue = Queue.queues.stream().filter(q -> q.getId().equals(server)).findFirst().get();
+                final String server = message;
+                final Queue queue = Queue.queues.stream().filter(q -> q.getId().equals(server)).findFirst().get();
 
                 if (queue.getQueued().size() == 0) return;
-                PlayerData first = PlayerData.players.get(queue.getQueued().getFirst());
+
+                final PlayerData first = PlayerData.players.get(queue.getQueued().getFirst());
 
                 queue.getQueued().parallelStream().map(uuid -> PlayerData.players.get(uuid)).forEach(data -> {
                     if (data != first) {
@@ -38,28 +41,18 @@ public class MessageListener implements Listener {
                     }
                 });
 
-                ByteArrayOutputStream b = new ByteArrayOutputStream();
-                DataOutputStream out = new DataOutputStream(b);
-
-                try {
-                    out.writeUTF("Connect");
-                    out.writeUTF(queue.getBungee());
-                } catch (IOException eee) {
-                    Bukkit.getLogger().info("You'll never see me!");
-                }
-
-                first.getPlayer().sendPluginMessage(HydroQueue.getInstance(), "BungeeCord", b.toByteArray());
-                first.getPlayer().sendMessage(Messages.sending(queue.getName()));
-
+                sendToServer(queue, first);
                 acknowledged(server);
+
                 break;
             }
             case "DENY": {
-                String[] split = message.split("@@");
-                String server = split[0];
-                String reason = split[1];
+                final String[] split = message.split("@@");
 
-                Queue queue = Queue.queues.stream().filter(q -> q.getId().equals(server)).findFirst().get();
+                final String server = split[0];
+                final String reason = split[1];
+
+                final Queue queue = Queue.queues.stream().filter(q -> q.getId().equals(server)).findFirst().get();
                 if (queue.getQueued().size() == 0) return;
 
                 switch (reason) {
@@ -74,9 +67,18 @@ public class MessageListener implements Listener {
                         break;
                     }
                     case "WHITELISTED": {
+                        final List<String> whitelisted = Arrays.asList(split[2].split(","));
+
                         queue.getQueued().parallelStream().map(uuid -> PlayerData.players.get(uuid)).forEach(data -> {
-                            int pos = QueueManager.getPlayerPos(data);
-                            int size = QueueManager.getQueued(data).getQueued().size();
+                            if (whitelisted.contains(data.getPlayer().getUniqueId().toString())) {
+                                // This is not the greatest fix, but it *should* work
+                                // Theoretically, there won't be that many whitelisted players all
+                                // in a queue.
+                                sendToServer(queue, data);
+                            }
+
+                            final int pos = QueueManager.getPlayerPos(data);
+                            final int size = QueueManager.getQueued(data).getQueued().size();
 
                             Messages.reminderFail(data.getPlayer(), "whitelisted", pos + 1, size);
                         });
@@ -86,9 +88,25 @@ public class MessageListener implements Listener {
                 }
 
                 acknowledged(server);
+
                 break;
             }
         }
+    }
+
+    private void sendToServer(final Queue queue, final PlayerData player) {
+        final ByteArrayOutputStream b = new ByteArrayOutputStream();
+        final DataOutputStream out = new DataOutputStream(b);
+
+        try {
+            out.writeUTF("Connect");
+            out.writeUTF(queue.getBungee());
+        } catch (IOException eee) {
+            Bukkit.getLogger().info("You'll never see me!");
+        }
+
+        player.getPlayer().sendPluginMessage(HydroQueue.getInstance(), "BungeeCord", b.toByteArray());
+        player.getPlayer().sendMessage(Messages.sending(queue.getName()));
     }
 
     private void acknowledged(String server) {
